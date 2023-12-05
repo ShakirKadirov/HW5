a9c0d402ccf77273799154556aa61fb6e0f02f2f
ShakirKadirov <shakirkadirov1@gmail.com>
2023-12-05 13:49:17 +0600

    Task #1 completed

--- HW5/main.swift
+++ HW5/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+// Task #1
 
+enum Season{
+    case winter
+    case spring
+    case summer
+    case autumn
+}
+
+
+func averageTemperature(season: Season) -> Int{
+    switch season {
+    case .winter:
+        return  -10
+    case .spring:
+        return 10
+    case .summer:
+        return 30
+    case .autumn:
+        return 5
+    default:
+        return 0
+    }
+}
+
+print(averageTemperature(season: .autumn))
